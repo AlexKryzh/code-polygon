7764de342ade631c3061b811adae4366d47146b0
Alex Kryzhanovskyy <alex@kryzh.com>
2021-12-27 09:45:31 +0100

    Add execute singleton code example

--- src/DesignPatterns/DesignPatternsSingleton.tsx
+++ src/DesignPatterns/DesignPatternsSingleton.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import Editor from '@monaco-editor/react';
 import Heading from 'carbon-react/lib/components/heading';
 import Detail from 'carbon-react/lib/components/detail';
@@ -5,17 +6,67 @@ import {
     Card,
     CardRow
   } from 'carbon-react/lib/components/card';
+import Box from 'carbon-react/lib/components/box';
+import Image from 'carbon-react/lib/components/image';
 
 function DesignPatternsSingleton() {
+    const [message, setMessage] = useState<string>('');
+
+    useEffect(() => {
+        class Singleton {
+            private static instance: Singleton;
+        
+            /**
+             * The Singleton's constructor should always be private to prevent direct
+             * construction calls with the "new" operator.
+             */
+            private constructor() { }
+        
+            /**
+             * The static method that controls the access to the singleton instance.
+             *
+             * This implementation let you subclass the Singleton class while keeping
+             * just one instance of each subclass around.
+             */
+            public static getInstance(): Singleton {
+                if (!Singleton.instance) {
+                    Singleton.instance = new Singleton();
+                }
+        
+                return Singleton.instance;
+            }
+        
+            /**
+             * Finally, any singleton should define some business logic, which can be
+             * executed on its instance.
+             */
+            public someBusinessLogic() {
+                // ...
+            }
+        }
+
+        const s1 = Singleton.getInstance();
+        const s2 = Singleton.getInstance();
+
+        if (s1 === s2) {
+            setMessage('Singleton works, both variables contain the same instance.');
+        } else {
+            setMessage('Singleton failed, variables contain different instances.');
+        }
+    }, []);
 
     return (
         <div>
             <Heading title="Singleton / Creational Design Pattern" />
-            <Card spacing="medium" cardWidth="300px">
-                <CardRow spacing="medium">
-                    <Detail>Singleton is a creational design pattern that lets you ensure that a class has only one instance, while providing a global access point to this instance.</Detail>
-                </CardRow>
-            </Card>
+            <Box display="flex" flexDirection="row" justifyContent="space-between" alignItems="stretch" mb={3}>
+                <Card spacing="medium" cardWidth="300px">
+                    <CardRow spacing="medium">
+                        <Detail>Singleton is a creational design pattern that lets you ensure that a class has only one instance, while providing a global access point to this instance.</Detail>
+                    </CardRow>
+                </Card>
+                <Image alt="Example alt text" src="/images/singleton.png" style={{height: '150px', width: 'auto'}} />
+            </Box>
+            
             <Editor
                 height="70vh"
                 defaultLanguage="typescript"
@@ -73,6 +124,11 @@ function clientCode() {
 clientCode();
                 `}
             />
+            <Box m={3} p={3} bg="secondary">
+                <Box height="100px" bg="primary" color="white" justifyContent="center" alignItems="center" display="flex">
+                    {message}
+                </Box>
+            </Box>
         </div>
       );
 }
--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -28,6 +28,61 @@ function Home() {
                             <Link href="/design-patterns-singleton">Singleton</Link>
                         </Box>
                     </Accordion>
+                    <Accordion title="Structural Design Patterns"  borders="none">
+                        <Box py={2}>
+                            <Link>Adapter</Link>
+                        </Box>
+                        <Box py={2}>
+                            <Link>Bridge</Link>
+                        </Box>
+                        <Box py={2}>
+                            <Link>Composite</Link>
+                        </Box>
+                        <Box py={2}>
+                            <Link>Decorator</Link>
+                        </Box>
+                        <Box py={2}>
+                            <Link>Facade</Link>
+                        </Box>
+                        <Box py={2}>
+                            <Link>Flyweight</Link>
+                        </Box>
+                        <Box py={2}>
+                            <Link>Proxy</Link>
+                        </Box>
+                    </Accordion>
+                    <Accordion title="Behavioral Design Patterns" borders="none">
+                        <Box py={2}>
+                            <Link>Chain of Responsibility</Link>
+                        </Box>
+                        <Box py={2}>
+                            <Link>Command</Link>
+                        </Box>
+                        <Box py={2}>
+                            <Link>Iterator</Link>
+                        </Box>
+                        <Box py={2}>
+                            <Link>Mediator</Link>
+                        </Box>
+                        <Box py={2}>
+                            <Link>Memento</Link>
+                        </Box>
+                        <Box py={2}>
+                            <Link>Observer</Link>
+                        </Box>
+                        <Box py={2}>
+                            <Link>State</Link>
+                        </Box>
+                        <Box py={2}>
+                            <Link>Strategy</Link>
+                        </Box>
+                        <Box py={2}>
+                            <Link>Template Method</Link>
+                        </Box>
+                        <Box py={2}>
+                            <Link>Visitor</Link>
+                        </Box>
+                    </Accordion>
                 </Accordion>
             </AccordionGroup>
         </div>
